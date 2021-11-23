841982af249a03cd2adc30c81823f2358a112c2f
Pavlo Kerestey <pavlo.kerestey@thoughtworks.com>
2021-11-23 21:59:21 +0100

    Use disintegration/imaging to generate thumbnails

--- thumbnail/generate.go
+++ thumbnail/generate.go
@@ -1,109 +1,91 @@
 package thumbnail
 
 import (
-  "fmt"
-  "image"
-  "image/jpeg"
-  "io/ioutil"
-  "log"
-  "os"
-  "path/filepath"
-  "strings"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
-  "github.com/andreaskoch/go-fswatch"
-  "github.com/edwvee/exiffix"
-  "github.com/nfnt/resize"
+	"github.com/andreaskoch/go-fswatch"
+	"github.com/disintegration/imaging"
 )
 
 func watchForChanges(originalsDir, thumbnailsDir string) {
-  recurse := true // include all sub directories
+	recurse := true // include all sub directories
 
-  skipDotFilesAndFolders := func(path string) bool {
-    return strings.HasPrefix(filepath.Base(path), ".")
-  }
+	skipDotFilesAndFolders := func(path string) bool {
+		return strings.HasPrefix(filepath.Base(path), ".")
+	}
 
-  checkIntervalInSeconds := 1
+	checkIntervalInSeconds := 1
 
-  folderWatcher := fswatch.NewFolderWatcher(
-    originalsDir,
-    recurse,
-    skipDotFilesAndFolders,
-    checkIntervalInSeconds,
-  )
+	folderWatcher := fswatch.NewFolderWatcher(
+		originalsDir,
+		recurse,
+		skipDotFilesAndFolders,
+		checkIntervalInSeconds,
+	)
 
-  folderWatcher.Start()
-  for folderWatcher.IsRunning() {
+	folderWatcher.Start()
+	for folderWatcher.IsRunning() {
 
-    select {
-    case changes := <-folderWatcher.ChangeDetails():
-      for _, file := range changes.New() {
-        thumbnailPath := thumbnailsDir + "/" + filepath.Base(file)
-        makeThumbnail(file, thumbnailPath)
-      }
-      for _, file := range changes.Modified() {
-        thumbnailPath := thumbnailsDir + "/" + filepath.Base(file)
-        makeThumbnail(file, thumbnailPath)
-      }
-      for _, file := range changes.Moved() {
-        thumbnailPath := thumbnailsDir + "/" + filepath.Base(file)
-        _ = os.Remove(thumbnailPath)
-      }
-    }
-  }
+		select {
+		case changes := <-folderWatcher.ChangeDetails():
+			for _, file := range changes.New() {
+				thumbnailPath := thumbnailsDir + "/" + filepath.Base(file)
+				makeThumbnail(file, thumbnailPath)
+			}
+			for _, file := range changes.Modified() {
+				thumbnailPath := thumbnailsDir + "/" + filepath.Base(file)
+				makeThumbnail(file, thumbnailPath)
+			}
+			for _, file := range changes.Moved() {
+				thumbnailPath := thumbnailsDir + "/" + filepath.Base(file)
+				_ = os.Remove(thumbnailPath)
+			}
+		}
+	}
 }
 
 func Generate(originalsDir, thumbnailsDir string, regenerate bool) {
-  files, err := ioutil.ReadDir(originalsDir)
-  if err != nil {
-    log.Fatal(err)
-  }
+	files, err := ioutil.ReadDir(originalsDir)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  if err := os.MkdirAll(thumbnailsDir, 0755); err != nil {
-    log.Fatal(err)
-  }
+	if err := os.MkdirAll(thumbnailsDir, 0755); err != nil {
+		log.Fatal(err)
+	}
 
-  for _, file := range files {
-    originalPicturePath := originalsDir + "/" + file.Name()
+	for _, file := range files {
+		originalPicturePath := originalsDir + "/" + file.Name()
 
-    if strings.Contains(strings.ToLower(originalPicturePath), ".jpg") || strings.Contains(strings.ToLower(originalPicturePath), ".jpeg") {
-      thumbnailPath := thumbnailsDir + "/" + file.Name()
+		if strings.Contains(strings.ToLower(originalPicturePath), ".jpg") || strings.Contains(strings.ToLower(originalPicturePath), ".jpeg") {
+			thumbnailPath := thumbnailsDir + "/" + file.Name()
 
-      if _, err := os.Stat(thumbnailPath); !(err == nil && regenerate == false) {
-        makeThumbnail(originalPicturePath, thumbnailPath)
-      }
-    }
-  }
+			if _, err := os.Stat(thumbnailPath); !(err == nil && regenerate == false) {
+				makeThumbnail(originalPicturePath, thumbnailPath)
+			}
+		}
+	}
 
-  watchForChanges(originalsDir, thumbnailsDir)
+	watchForChanges(originalsDir, thumbnailsDir)
 }
 
 func makeThumbnail(originalPicturePath, thumbnailPath string) {
-  file, err := os.Open(originalPicturePath)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
+	src, err := imaging.Open(originalPicturePath, imaging.AutoOrientation(true))
+	if err != nil {
+		log.Fatalf("failed to open original: %v", err)
+	}
 
-  picture, _, err := exiffix.Decode(file)
-  if err != nil {
-    log.Fatal(err)
-  }
+	thumbnail := imaging.Thumbnail(src, 200, 200, imaging.Box)
 
-  pictureDimensions := picture.Bounds()
+	err = imaging.Save(thumbnail, thumbnailPath, imaging.JPEGQuality(80))
+	if err != nil {
+		log.Fatalf("failed to save thumbnail: %v", err)
+	}
 
-  var resized image.Image
-  if pictureDimensions.Dx() > pictureDimensions.Dy() {
-    resized = resize.Resize(0, 200, picture, resize.NearestNeighbor)
-  } else {
-    resized = resize.Resize(200, 0, picture, resize.NearestNeighbor)
-  }
-
-  outputFile, err := os.Create(thumbnailPath)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer outputFile.Close()
-
-  jpeg.Encode(outputFile, resized, nil)
-  fmt.Print(".")
+	fmt.Print(".")
 }
